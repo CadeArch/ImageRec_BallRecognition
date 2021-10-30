eb3753fabeaf1a0a38963e5cf784673fbfa0f033
Cade Rasmussen <rascade@gmail.com>
2021-10-30 13:59:33 -0600

    structure down

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
 import cv2
 
-if __name__ == "__main.py__":
 
-    # -d configure which camera device to use
-    # -r Size of foto
-    # -S visibility range 1 - 10 if parameter not set or set to 0 foto will be black
-    # if path not specified /home/pi is the default
-    # EXAMPLE: "fswebcam -d /dev/video0 --no-banner -r 320x240 -S 10 /home/pi/foto.jpg
 
+# -d configure which camera device to use
+# -r Size of foto
+# -S visibility range 1 - 10 if parameter not set or set to 0 foto will be black
+# if path not specified /home/pi is the default
+# EXAMPLE: "fswebcam -d /dev/video0 --no-banner -r 320x240 -S 10 /home/pi/foto.jpg
+def main():
     dirFoto = "/home/pi/"
     imageName = "camView.jpg"
     commandToTakeFoto = "fswebcam -d /dev/video0 --no-banner -r 320x240 -S 10 " + dirFoto + imageName
+    print("hi")
+
+
+if __name__ == "__main__":
+    main()
 
 
